09e44b9ee1baaa4150faf2574015c01d99f783f4
Silas Loo <silas@MacBook-Pro.local>
2026-02-06 13:46:18 +0800

    Update homepage headline

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -60,7 +60,7 @@ const home: Home = {
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Building bridges between design and code</>,
+  headline: <>I design brands that move people — and businesses forward</>,
   featured: {
     display: true,
     title: (
